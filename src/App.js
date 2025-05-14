80129347500834daafa9afcf79ebb6103dc649cf
Calum <calumjamescraig@gmail.com>
2025-05-14 17:50:40 +0100

    Created app and implemented fetch function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import {useState, useEffect} from "react"; 
+
+const URL = "https://api.funtranslations.com/translate/pirate.json?text=Hello World how are you, you inexperienced sailor?";
 
 function App() {
+  
+  const [translation, setTranslation] = useState(0)
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(URL)
+      result.json().then(json => {
+        console.log(json);
+        setTranslation(json.contents.translated);
+      })
+      
+  
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Pirate translator</h1>
       </header>
+      <h2>Enter yer text, landlubber!</h2>
+      <input type="text" />
+      <button type="submit">Submit</button>
+      <h3>Translated text: {translation}</h3>
     </div>
   );
 }
