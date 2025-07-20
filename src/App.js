f8baab3a77f70f7a7473452d7f49321729149f88
Calum <calumjamescraig@gmail.com>
2025-07-20 17:57:37 +0100

    Edited

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, {useState, useEffect} from "react";
 import TranslatorContainer from "./containers/TranslatorContainer";
 
-//const URL = "https://api.funtranslations.com/translate/pirate.json?text=Hello World how are you, you inexperienced wanker?";
+//const URL = "https://api.funtranslations.com/translate/pirate.json?text=Hello World how are you?";
 
 function App() {
   
